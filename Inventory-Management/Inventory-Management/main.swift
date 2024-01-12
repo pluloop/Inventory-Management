a0246ab63e105e11fe283ffa0443d5826ecb1e57
Andy Nguyen <andynguyen852@gmail.com>
2024-01-12 10:28:09 -0800

    Add files via upload

---
+++ Inventory-Management/Inventory-Management/main.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  Inventory-Management
+//
+//  Created by StudentAM on 1/12/24.
+//
+
+import Foundation
+
+func mainMenu(){
+    print("1. Add item to cart\n" +
+          "2. Remove item for cart\n" +
+          "3. Check if item is in stock\n" +
+          "4. Admin Menu\n" +
+          "5. Checkout\n")
+}
+
+mainMenu()
